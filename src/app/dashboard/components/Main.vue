a135e7939d238cf55d5706a38b079da51bf1aede
Luciano Baraúna <baraunaluciano@gmail.com>
2018-09-10 17:44:38 -0300

    finalizada aula 11

--- src/app/dashboard/components/Main.vue
+++ src/app/dashboard/components/Main.vue
@@ -2,14 +2,21 @@
     <div>
         <header class="page-header row">
             <h2>Olá!, {{$route.params.name}}</h2>
-
+            <!-- <pre>{{}}</pre> -->
         </header>
     </div>
 </template>
 
 <script>
+import http from '@/http'
 export default {
-
+  mounted () {
+    http.get('http://pokeapi.co/api/v2/pokemon/1/')
+      .then(response => response.data)
+      .then(data => {
+        console.log(data)
+      })
+  }
 }
 </script>
 
